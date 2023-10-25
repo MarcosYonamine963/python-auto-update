b036fbfefc0f151f566bbb0ac320c024af34a040
MarcosYonamine963 <marcos.m.yonamine@gmail.com>
2023-10-25 20:58:22 -0300

    verify for new update on main before showing notification

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import calculator
+import VerifyNewUpdate
 import sys
 import os
 import subprocess
@@ -7,50 +8,63 @@ import time
 from PyQt5.QtWidgets import QApplication, QMessageBox
 
 
-def ask_for_update():
+__version__ = "v0.0.1"
+latest_version = __version__
+repo_owner = "MarcosYonamine963"
+repo_name = "python-auto-update"
+
+def ask_for_update(version):
     msg = QMessageBox()
     msg.setWindowTitle("Updater")
     msg.setIcon(QMessageBox.Question)
-    msg.setText("Search for new updates?")
+    msg.setText(f"New version found: {version}. Update?")
     msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
     return msg.exec()
 
-if __name__ == "__main__":
-    __version__ = "v0.0.2"
 
+def run_main_app():
     app = QApplication(sys.argv)
+    window = calculator.CalculatorApp(__version__)
+    window.show()
+    sys.exit(app.exec_())
 
-    result = ask_for_update()
 
-    if(result == QMessageBox.Yes):
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    latest_version = VerifyNewUpdate.verify_new_update(__version__, repo_owner, repo_name)
+    
+    if latest_version != __version__ and latest_version != 0:
+        result = ask_for_update(latest_version)
 
-        # update json file
-        json_data = {
-            "current_version": f"{__version__}"
-        }
+        if(result == QMessageBox.Yes):
 
-        with open('version.json', 'w') as v_file:
-            json.dump(json_data, v_file, indent=4)
+            # update json file
+            json_data = {
+                "current_version": f"{__version__}"
+            }
 
-        # Start updater
-        try:
-            subprocess.Popen("./updater")
-            time.sleep(1)
-            # sys.exit()
-        except:
-            msg = QMessageBox()
-            msg.setWindowTitle("Error")
-            msg.setIcon(QMessageBox.Critical)
-            msg.setText("Updater not Found")
-            msg.exec()
+            with open('version.json', 'w') as v_file:
+                json.dump(json_data, v_file, indent=4)
+
+            # Start updater
             try:
-                os.remove('version.json')
+                subprocess.Popen("./updater")
+               
             except:
-                pass
+                msg = QMessageBox()
+                msg.setWindowTitle("Error")
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText("Updater not Found")
+                msg.exec()
+                run_main_app()
+                try:
+                    os.remove('version.json')
+                except:
+                    pass
 
 
+        else:
+            run_main_app()
+    
     else:
-
-        window = calculator.CalculatorApp(__version__)
-        window.show()
-        sys.exit(app.exec_())
+        run_main_app()
---
+++ tests/VerifyNewUpdate.py
@@ -0,0 +1,51 @@
+import sys
+import requests
+
+def verify_new_update(version, repo_owner, repo_name):
+# Connect to github api and check for new release version (tag name)
+# Return -1 if error; 0 if there is no update; 1 if there is update.
+
+    print("Starting: verify_new_update()")
+    print("Connecting to server")
+
+    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
+
+    try:
+        request_info_response = requests.get(api_url)
+
+    except requests.exceptions.ConnectionError:
+        print("CONNECTION FAIL")
+        print("Check network connection, and try again")
+        return -1
+
+    except:
+        print("UNKNOWN ERROR")
+        return -1
+
+    else:
+        if request_info_response.status_code == 404:
+            print("Server not found or Offline")
+            print("Contact Support")
+            return -1
+        elif request_info_response.status_code == 200:
+            print("Connection Successful")
+
+            print("Searching for Updates")
+            release_data = request_info_response.json()
+            latest_version = release_data["tag_name"]
+
+            if(latest_version != version):
+                print(f"New version found: {latest_version}")
+                return 1
+
+            else:
+                print("Already on latest version!")
+                return 0
+
+
+if __name__ == "__main__":
+    __version__ = "v0.0.1"
+    repo_owner = "MarcosYonamine963"
+    repo_name = "python-auto-update"
+
+    verify_new_update(__version__, repo_owner, repo_name)
